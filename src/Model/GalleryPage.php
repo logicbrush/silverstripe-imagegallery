1113155fac1d32b6e3c13a7b346103b9d71363b6
Michael Nowina-Krowicki <michael@3dgoo.com>
2020-05-20 16:25:51 +0930

    Moving Gallery Page Content to a template so that it can overwritten by users if needed

--- src/Model/GalleryPage.php
+++ src/Model/GalleryPage.php
@@ -7,7 +7,7 @@ use SilverStripe\Assets\Image;
 use SilverStripe\Core\ClassInfo;
 use SilverStripe\Forms\Tab;
 use SilverStripe\View\Requirements;
-
+use SilverStripe\View\SSViewer;
 
 class GalleryPage extends \Page {
 
@@ -89,24 +89,16 @@ class GalleryPage extends \Page {
 		$content = $this->Content;
 
 		if ($this->Images()) {
-			$pos = 0;
-			$content .= "<div class='image-gallery' itemscope itemtype='http://schema.org/ImageGallery'>";
-			foreach($this->SortedImages() as $image) {
-				$content .= "<figure class='item' itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject' data-index='{$pos}'>";
-				$content .= "<a href='{$image->FitMax(2000,2000)->URL}' itemprop='contentUrl' data-width='{$image->FitMax(1000,1000)->Width}' data-height='{$image->FitMax(1000,1000)->Height}' data-index='{$pos}' aria-label='{$this->he($image->Title)}'>";
-				$content .= "<img src='{$image->FocusFill(480,480)->URL}' width='480' height='480' itemprop='thumbnail' alt='{$this->he($image->Title)}' />";
-				$content .= "</a>";
-				$content .= "</figure>";
-				++$pos;
-			}
-			$content .= "</div>";
+			$template = new SSViewer('Logicbrush/ImageGallery/Includes/GalleryPageContent');
+
+			$data = [
+				'Images' => $this->SortedImages(),
+			];
+
+			$content .= $template->process($this->controller, $data);
 		}
 
 		return $content;
-	}
-
-	private function he($str) {
-		return htmlentities($str);
 	}
 
 }
